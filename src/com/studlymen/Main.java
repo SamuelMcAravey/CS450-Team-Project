3b4966b1e32e91077b5bb83e4322707de15ba463
Samuel McAravey <mcaravey@rixian.net>
2015-05-13 13:14:39 -0600

    Added empty java project with Weka.

---
+++ src/com/studlymen/Main.java
@@ -0,0 +1,14 @@
+package com.studlymen;
+
+import weka.core.converters.*;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        TextDirectoryLoader loader = new TextDirectoryLoader();
+
+        System.out.print("Hello Studly Men!");
+    }
+}
