3c0907ad5048e392c2ee306b5abba2427ac38b69
Samuel McAravey <mcaravey@rixian.net>
2015-07-06 14:47:53 -0600

    Added missing jar file.

---
+++ src/com/studlymen/LdaClassifier.java
@@ -0,0 +1,12 @@
+package com.studlymen;
+import cc.mallet.topics.HierarchicalLDA;
+
+/**
+ * Created by samuelmcaravey on 7/6/15.
+ */
+public class LdaClassifier
+{
+    public LdaClassifier()
+    {
+    }
+}
